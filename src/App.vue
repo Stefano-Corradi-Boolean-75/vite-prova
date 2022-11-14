5f18ad889aeaa3c8fc262c722377f81862697334
ugodeughi <stefano.corradi@gmail.com>
2022-11-14 13:13:19 +0100

    fine LC inizializzazine vite

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import PrimoComponent from './components/PrimoComponent.vue'
+
+export default {
+  name: 'App',
+  components:{
+    PrimoComponent
+  },
+  data(){
+    return{
+      titolo: 'Ciao Vue Vite da data'
+    }
+  }
+}
+</script>
+
+<template>
+
+  <div class="container">
+    <h1>{{titolo}}</h1>
+    <h2>Sono un H2</h2>
+  </div>
+  <PrimoComponent />
+    
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/PrimoComponent.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: 'PrimoComponent',
+  data(){
+    return{
+      titolo: 'sono il titolo del componente'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h2>{{titolo}}</h2>
+  </div>
+</template>
+
+
+
+<style scoped>
+h2{
+  color: red;
+}
+</style>
